7b6cf1fcab0c159a78ef8128d4724a1025e12eab
aonosorah <102835599+aonosorah@users.noreply.github.com>
2022-05-09 14:49:53 -0300

    projeto javascript

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,66 @@
+// Parte 1
+// 1 - 10 10
+// 2 - 10 10 10
+// 3 - Varia de acordo com as informações fornecidas
+/* Com uma carga horária de 8 horas por dia
+recebendo 35 reais por hora.
+O valor da hora recebida fica em R$ 4,75 aproximadamente*/
+
+// Parte 2
+
+//Exercício 1
+
+// prompt("Qual seu nome?")
+// console.log 
+// prompt("Qual sua idade?")
+// console.log
+// const nome = (prompt ("Qual seu nome?"))
+// console.log
+// typeof nome
+// console.log (typeof nome)
+// const idade = (prompt ("Qual sua idade?"))
+// console.log
+// typeof idade
+// console.log (typeof idade)
+
+//informa que a idade e nome são strings. mas não mostra como número ou nome.
+
+// prompt ("Qual sua idade?")
+// const idade = 33
+// console.log (typeof idade)
+
+// informa que a resposta contida é um número.
+
+const nome = "Mayara";
+const sobrenome = "Costa";
+const idade = 33
+console.log("Hi, my name is,", nome, sobrenome, "I am", idade)
+typeof idade
+console.log(typeof idade)
+
+//adiciona uma frase ao console contendo as informações enviadas pela 'const'
+
+// Exercício 2
+
+// let sim = (prompt ("Você gosta de música?"))
+// let não = (prompt ("Você gosta de música?"))
+// console.log 
+
+
+// let s = (prompt ("você gosta de sair?"))
+// let n = (prompt ("você gosta de sair?"))
+// console.log
+
+// let yes = (prompt ("você é uma pessoa diurna?"))
+// let no = (prompt ("você é uma pessoa diurna?"))
+// console.log 
+
+// Exercício 3
+
+let a = 5
+let b = 10
+let c = a+b
+console.log("O novo valor de a é", b) 
+console.log("O novo valor de b é", a) 
+console.log("O novo valor de c é", a+b)
+
