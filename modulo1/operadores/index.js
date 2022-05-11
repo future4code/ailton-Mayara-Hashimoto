41763a23c4f5347d4a14fca672c8429cf5f180ec
aonosorah <102835599+aonosorah@users.noreply.github.com>
2022-05-11 16:10:55 -0300

    string e arrays

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,76 @@
+// # PARTE 1
+// Exercício 1
+// Resultado - False | False | True | Boolean
+
+// Ecercício 2
+
+// let primeiroNumero = prompt("Digite um numero!")
+// let segundoNumero = prompt("Digite outro numero!")
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+
+// O console está unindo os números, não somando.
+
+// Exercício 3
+
+// let primeiroNumero = Number (prompt("Digite um numero!"))
+// let segundoNumero = Number (prompt("Digite outro numero!"))
+// let resultado = primeiroNumero + segundoNumero
+// const soma = (resultado)
+
+// console.log(resultado)
+
+// Transformar o string em número, criar uma variante não constante, aplicar o resultado para constante
+ 
+// PARTE 2
+
+// Exercício 1
+
+// let primeiroNumero = Number (prompt("Qual sua idade?"))
+// let segundoNumero = Number (prompt("Qual a idade do seu melhor amigo(a)?"))
+// const maisVelho = primeiroNumero >= segundoNumero
+// console.log (maisVelho)
+// const resultado = primeiroNumero - segundoNumero
+// console.log = (resultado)
+// respsta aparece como true no caso 33 > 25, sim, o usuário é mais velho que o melhor amigo.
+
+
+// Exercício 2
+// const primeiroValor = Number(prompt("Informe um número qualquer"))
+// const valorDivisor = 2
+
+// const resultado = primeiroValor % valorDivisor
+
+// console.log(resultado)
+// Os numeros pares irão aparecer como 0 e os impares como 1
+
+//Exercício 3
+
+// let idade = Number(prompt("Digite sua idade. Por favor."))
+// let meses = idade * 12
+// console.log(meses)
+// let dias = meses * 30
+// console.log(dias)
+// let horas = dias * 24
+// console.log(horas)
+
+// console.log(meses,  dias, horas)
+
+//Exercício 4
+
+let nUm = Number (prompt ("Digite um número, por favor."))
+let nDois = Number (prompt ("Mais um número, por favor."))
+const condicao1 = nUm > nDois
+const condicao2 = nUm === nDois
+console.log(condicao1, condicao2)
+
+const condicao3 = nUm % nDois
+let division1 = condicao3 === 0
+console.log(condicao3 % division1)
+const condicao4 = nDois % nUm
+let division2 = condicao4 === 0
+console.log(condicao4, division2)
+
+//
