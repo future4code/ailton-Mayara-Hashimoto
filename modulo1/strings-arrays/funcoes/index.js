1fce82f167352e1268e0bb11e3dc642d10183ca1
aonosorah <102835599+aonosorah@users.noreply.github.com>
2022-05-12 16:51:55 -0300

    some are not finished

---
+++ modulo1/strings-arrays/funcoes/index.js
@@ -0,0 +1,57 @@
+// PARTE 1
+// Exercício 1
+// a) Será impresso o valor da multiplicação, 10 e 50.
+// b) O mesmo resultado é impresso da mesma forma.
+
+// Exercício 2
+// a) Vai me informar se no texto fornecido do usuário consta a palavra cenoura ou não no sistema booleano.
+//    caso conste, true, caso não, false.
+// b) Todas serão impressas como true, por conter 'cenoura' e o return aprensentando o comando toLowerCase
+
+// PARTE 2
+
+// Ecercício 1
+// a)
+// function phrase() {
+//     console.log("Eu sou Mayara, tenho 33 anos, moro em São Paulo e sou estudante")
+
+// }
+// phrase()
+
+// b)
+// function  phrase (name, age, address, occupation){
+//      name = prompt("Name") 
+//      age = prompt Number ("Age")      
+//      address = prompt("Address")
+//      occupation = prompt("Occupation")
+//      console.log(`Hi, my name is " ${name}, "I am "${age}, "I live at " ${address}, "my occupation is " ${occupation}`)
+// }
+// console.log(function)
+
+// Exercício 2 
+// // a) 
+const num1 = 6
+const num2 = 8
+function soma(num1,num2) {
+  const soma = num1 + num2
+    return soma
+}
+console.log(soma(num1,num2))
+
+// // // b)
+const fu = (a,b) => a >= b
+console.log(fu (6, 8))
+
+// c)
+const dd = num1 % 2
+console.log(dd)
+
+//d)
+ name = "MAYARA"
+function change(name) {
+    name = name.toLowerCase()
+    return name
+}
+console.log(name)
+
+// EXERCÍCIO 3
