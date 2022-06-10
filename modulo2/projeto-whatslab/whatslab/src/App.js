b3f381710f1880c94c0aef2f81a06d8b002c5183
aonosorah <102835599+aonosorah@users.noreply.github.com>
2022-06-10 20:39:40 -0300

    caixa de mensagem estilizada

--- modulo2/projeto-whatslab/whatslab/src/App.js
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "./App.css"
+import styles  from './components/styles.js'
 
 
 class App extends React.Component {
@@ -7,15 +8,15 @@ class App extends React.Component {
     
     usuario: [
       {
-        nome: "Bang Chan",
-        message: "Welcome to The Wolfgang Chat!!"
+        nome: <img className="profile" src='https://i.pinimg.com/736x/22/b1/f7/22b1f70c8866bf10ccfea6ab21ec5db1.jpg' />,
+        message: <img src='https://i.im.ge/2022/06/11/rzZk7h.jpg' />
       },
       {
-        nome: "Hyunjin",
+        nome: <img className="profile" src='https://i.pinimg.com/originals/b8/45/ec/b845ec72d1c359eba9a3a74b271bf53c.jpg' />,
         message: "Hi babyy..."
       },  
       {
-        nome: "Yang Jeong In",
+        nome: <img className="profile" src='https://i.pinimg.com/originals/17/74/d3/1774d302358bbe55cc10feeaf6d6d3b6.jpg' />,
         message: "Hello!!!"
       }
 
@@ -68,6 +69,7 @@ class App extends React.Component {
         <div>
         <p className="box">{historico}</p>
         <footer>
+          
           <input className="boxdois"
             value={this.state.valorInputUsuario}
             onChange={this.onChangeInputUsuario}
