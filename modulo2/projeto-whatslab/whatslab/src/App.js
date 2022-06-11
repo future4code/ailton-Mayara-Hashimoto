408b88d40954087570b9ce013fa6d04a8d8a71c3
aonosorah <102835599+aonosorah@users.noreply.github.com>
2022-06-11 18:57:34 -0300

    mudanças felizes

--- modulo2/projeto-whatslab/whatslab/src/App.js
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -1,25 +1,107 @@
 import React from "react";
-import styled from "./App.css"
-import styles  from './components/styles.js'
+import styled from 'styled-components'
+// import styled from "./App.css"
 
+
+
+const Container = styled.div `
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    border: 1px solid #ffe4c4;
+    background-color: beige;
+    `
+const Title = styled.h1 `
+    color: #dda0dd;
+    display: flex;
+    justify-content: center;
+
+`
+const Messages = styled.div`
+  width: 500px;
+  height: 660px;
+  margin: 5px auto;
+  background-color: #D8C39A;
+  padding: 5px;
+  text-align: flex-start;
+  color: rgb(0, 0, 0);
+  `
+  const Users = styled.img `
+    border-radius: 4px;
+    align-items: center;
+    justify-content: center;
+  `
+const Username = styled.input `
+    display: flex;
+    border: 2px solid rgb(194, 148, 148);
+    border-radius: 4px;
+    justify-content: space-between;
+    width: 100px;
+    height: 30px;
+  `
+const MessageBox = styled.input `
+    display: flex;
+    border: 2px solid rgb(194, 148, 148);
+    border-radius: 4px;
+    justify-content: space-between;
+    align-items: center;
+    align-content: center;
+    width: 250px;
+    height: 30px;
+`
+const Footer = styled.footer `
+    display: flex;
+    justify-content: center;
+    position: bottom;
+`
+const Button = styled.button `
+    background-color: #dda0dd;
+    border: none;
+    border-radius: 4px;
+    color: white;
+    padding: 4px 15px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+`
+const Text = styled.div`
+background-color: whitesmoke;
+border-radius: 4px;
+align-items: center;
+flex-direction: column;
+width: auto;
+height: 5%;
+`
+const UserMessage = styled.div`
+  flex-direction: column;
+  color: black;
+  padding: 0.5px 5px;
+  background-color: #dda0dd;
+  border-radius: 10px;
+  margin: 5px;
+`
 
 class App extends React.Component {
   state = {
     
     usuario: [
       {
-        nome: <img className="profile" src='https://i.pinimg.com/736x/22/b1/f7/22b1f70c8866bf10ccfea6ab21ec5db1.jpg' />,
-        message: <img src='https://i.im.ge/2022/06/11/rzZk7h.jpg' />
+        nome: <Users src='https://i.im.ge/2022/06/12/rHjSIx.png' />,
+        message: <Text>Welcome to BubblesChat Stay! Here you can actually talk to us when we're avaliable! For a limited amout of time!</Text>
       },
       {
-        nome: <img className="profile" src='https://i.pinimg.com/originals/b8/45/ec/b845ec72d1c359eba9a3a74b271bf53c.jpg' />,
-        message: "Hi babyy..."
+        nome: <Users src='https://i.im.ge/2022/06/12/rHj7PS.png' />,
+        message: <Text>Hi Stayy...</Text>
       },  
       {
-        nome: <img className="profile" src='https://i.pinimg.com/originals/17/74/d3/1774d302358bbe55cc10feeaf6d6d3b6.jpg' />,
-        message: "Hello!!!"
+        nome: <Users src='https://i.im.ge/2022/06/12/rHjady.png' />,
+        message: <Text>Hello!!!</Text>
+      }, 
+      {
+        nome: <Users src='https://i.im.ge/2022/06/12/rHjqJJ.png' />,
+        message: <Text>STAAAY!!!</Text>
       }
-
     ],
     
     valorInputUsuario: "",
@@ -56,35 +138,42 @@ class App extends React.Component {
 
     const historico = this.state.usuario.map((user) => {
       return (
-        <p>
-          {user.nome} - {user.message}
-        </p>
+        
+        <UserMessage>
+         <strong> {user.nome} </strong>
+         <p> {user.message} </p>
+        </UserMessage>
       );
     });
 
+
     return (
-      <div className="container">
+      <Container>
        
-        <h1 className="header">Bubbles: Wolfgang Chat</h1>
+        <Title>Bubbles: SKZ [Wolfgang Chat]</Title>
         <div>
-        <p className="box">{historico}</p>
-        <footer>
           
-          <input className="boxdois"
+        <Messages>{historico}</Messages>
+        
+        <Footer>
+          
+          <Username
             value={this.state.valorInputUsuario}
             onChange={this.onChangeInputUsuario}
             placeholder={"Usuario"}
           />
-          <input className="boxtres"
+
+          <MessageBox
             value={this.state.valorInputMessage}
             onChange={this.onChangeInputMessage}
             placeholder={"Message"}
           />
-          <button onClick={this.adicionaUsuario}>Enviar</button>
-          </footer>
+
+          <Button onClick={this.adicionaUsuario}>Enviar</Button>
+          </Footer>
         </div>
         
-      </div>
+      </Container>
     );
   }
 }
