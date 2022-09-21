6105c0b6776340175e90cb832415fb2b7c14de70
aonosorah <102835599+aonosorah@users.noreply.github.com>
2022-09-21 14:59:18 -0300

    signup e login

--- modulo5/arquitetura-camadas/src/business/UserBusiness.ts
+++ modulo5/arquitetura-camadas/src/business/UserBusiness.ts
@@ -1,8 +1,16 @@
-import { Users } from "../models/User";
+import { Login, User, Users, USER_ROLES } from "../models/User";
 import { MissingFields } from "../error/missingFields";
+import { UserDatabase } from "../database/UserDatabase";
+import { Authenticator } from "../services/Authenticator";
+import { HashManager } from "../services/HashManager";
+import { IdGenerator } from "../services/IdGenerator";
 
 
 export class UserBusiness {
+    constructor (
+        private userDataBase:UserDatabase, private authenticator:Authenticator,
+        private hashManager:HashManager, private idGenarator:IdGenerator
+    ) {    }
     public signup = async (users:Users):Promise<any> => {
         const {name, email, password}:Users = users;
         
@@ -10,7 +18,34 @@ export class UserBusiness {
         if (password.length < 3) {
             throw new Error("Password must have more than 3 carachters")
         }
-        
-        return ""
+        const verify = await this.userDataBase.verifyEmail(email)
+        if (verify) {
+            throw new Error("Email already exists")
+        }
+        const id = this.idGenarator.generate()
+        const hashPass = await this.hashManager.hash(password)
+        const role = USER_ROLES.NORMAL
+        const user = new User(id, name, email, hashPass, role)
+        await this.userDataBase.signup(user)
+
+        const token = this.authenticator.generateToken({id, role})
+        return {token, role}
+    }
+    public login = async (user:Login):Promise<string> => {
+        const {email, password}:Login = user;
+        if(!email || !password) throw new MissingFields();
+        if (password.length < 3) {
+            throw new Error("Password must have more than 3 carachters")
+        }
+        const verify = await this.userDataBase.verifyEmail(email)
+        if (!verify) {
+            throw new Error("This email does not exist")
+        }
+        const verifyToken = await this.hashManager.compare(password, verify.getPassword())
+        if (!verifyToken) {
+            throw new Error("Password is INCORRECT")
+        }
+        const token = this.authenticator.generateToken({id: verify.getId(), role: verify.getRole()})
+        return token
     }
 }
--- modulo5/arquitetura-camadas/src/controller/UserController.ts
+++ modulo5/arquitetura-camadas/src/controller/UserController.ts
@@ -3,14 +3,27 @@ import { UserBusiness } from "../business/UserBusiness";
 import { Users } from "../models/User"
 
 export class UserController {
+    constructor ( 
+        private userBusiness:UserBusiness
+    ) {}
     public signup = async (req:Request, res: Response) => {
         try {
             const { name, email, password }:Users = req.body;
-            const uBusiness = new UserBusiness();
             const nUser:Users = { name, email, password }
-            const data = await uBusiness.signup(nUser);
+            const data = await this.userBusiness.signup(nUser);
 
             res.status(200).send({message: "User created.", data})
+        }
+        catch (err:any) {
+            res.status(err.statusCode || 500).send({message: err.message})
+        }
+    }
+    public login = async (req:Request, res:Response) => {
+        try {
+            const { email, password } = req.body
+            const user = { email, password }
+            const token = await this.userBusiness.login(user)
+            res.status(200).send({token})
         }
         catch (err:any) {
             res.status(err.statusCode || 500).send({message: err.message})
